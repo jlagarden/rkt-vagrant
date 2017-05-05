3d5eb04fd3f7677a4d17542e92ce9122da70e2e2
Ubuntu <ubuntu@ubuntu-yakkety.localdomain>
2017-05-05 22:06:13 +0000

    create keys

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd /vagrant
+
+apt-get update && apt-get install -y rkt acbuild rng-tools expect
+
 echo 'HRNGDEVICE=/dev/urandom' >> /etc/default/rng-tools
 
 NAME="Carly Container"
@@ -29,4 +33,19 @@ gpg --batch --gen-key gpg-batch
 
 HASH="$(gpg --no-default-keyring --secret-keyring ./rkt.sec --keyring ./rkt.pub --list-keys | awk '{print NR-1 "," $0}' | grep '3, *' | awk '{print $2;}')"
 
-gpg --yes --no-default-keyring --secret-keyring ./rkt.sec --keyring ./rkt.pub --edit-key $HASH trust 5 quit
+/usr/bin/expect <<EOF
+spawn echo "$HASH"
+spawn gpg --yes --no-default-keyring --secret-keyring ./rkt.sec --keyring ./rkt.pub --edit-key $HASH trust quit
+
+expect "Your decision?"
+send "5\r"
+
+expect "Do you really want to set this key to ultimate trust? (y/N)"
+send "y\r"
+
+EOF
+
+gpg --no-default-keyring --armor --secret-keyring ./rkt.sec --keyring ./rkt.pub --export $EMAIL > pubkeys.gpg
+gpg --export-secret-keys --secret-keyring ./rkt.sec --keyring ./rkt.pub  $HASH > secret.asc
+
+
